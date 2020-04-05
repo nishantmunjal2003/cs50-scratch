5665e4904201247d56fc4ffa723ab67e96510129
nishantmunjal2003 <nishant@gkv.ac.in>
2020-04-05 15:56:39 +0530

    no message

--- mario.c
+++ mario.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
+int main(void)
 {
-    int n;
-    printf("Enter the number of lines: ");
-    scanf("%d",&n);
     
+    int n;
+    do
+    {
+        printf("Input a valid number (1-8) of lines: ");
+        scanf("%d",&n);
+
+    }while(n==0 || n=="" || n<1 || n>8);
     for(int i=0; i<n+1; i++)
     {
         for(int j=0;j<i; j++)
